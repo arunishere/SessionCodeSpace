89db98a7af59cf3c4ae62e79e1205b0aec7cec86
arun <arunkumarsatish@gmail.com>
2024-07-02 09:37:01 +0530

    02-07-2024

--- Test.py
+++ Test.py
@@ -1,17 +1,42 @@
-from stack import Stack
+# from stack import Stack
 
-elements = ['a'+ str(i) for i in range(8)]
+# elements = ['a'+ str(i) for i in range(8)]
 
-s = Stack()
+# s = Stack()
 
-for element in elements:
+# for element in elements:
 
-    s.push(element)
+#     s.push(element)
 
 
-print(s.pop())
+# print(s.pop())
 
 
 # print(elements)
 
+from queuemain import Queue
 
+
+q = Queue(10)
+
+elements = ['a'+ str(i) for i in range(12)]
+
+for ele in elements:
+
+    q.enqueue(ele)
+
+print(q.viewQueue())
+
+print(q.dequeue())
+
+print(q.viewQueue())
+
+q.enqueue('a20')
+
+print(q.viewQueue())
+
+q.enqueue('a21')
+
+# while not q.isEmpty():
+
+#     print(q.dequeue())
---
+++ queuemain.py
@@ -0,0 +1,42 @@
+class Queue:
+    
+    def __init__(self, maximum):
+        
+        self.maximum = maximum
+        self.values = []
+
+    def enqueue(self, element):
+
+        if self.size() == self.maximum:
+
+            print('Queue is full, cannot add elements')
+
+        else:
+
+            self.values.append(element)
+
+
+    def dequeue(self):
+
+        if self.isEmpty():
+
+            print('Queue is Empty')
+
+        else:
+
+            return self.values.pop(0)
+    
+
+    def isEmpty(self):
+
+        return len(self.values) == 0
+    
+
+    def size(self):
+
+        return len(self.values)
+    
+
+    def viewQueue(self):
+
+        return self.values
